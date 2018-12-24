ffc55b6d106cf42886c1919f702e564f1e5a4951
Milos Antic <sarakrstic03@gmail.com>
2018-12-25 00:54:20 +0100

    One Corporation Design

---
+++ js/custom.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+    $('.home_slider').slick({
+        dots: true,
+        arrows: true
+    });
+    $('.who_we_are .slider').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.projects .slider').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+
+    $('.clients .slider').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+
+    });
+
+    $('.top').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    })
+
+    $('.home_slider .slick-dots').addClass('container');
+
+    /* map */
+    // When the window has finished loading create our google map below
+    google.maps.event.addDomListener(window, 'load', init);
+
+    function init() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 15,
+
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(45.0451719, 20.0838736), // New York
+
+        };
+
+        // Get the HTML DOM element that will contain your map
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(45.0451719, 20.0838736),
+            map: map,
+            title: 'Google Map Marker!!'
+        });
+    }
+
+});
